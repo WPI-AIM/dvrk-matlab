ebaa064d633abc966f35450d53efb54a014e0cfc
Adnan Munawar <amunawar@wpi.edu>
2014-12-01 15:47:09 -0500

    Added Custom Message Listener

--- psm1_listener.m
+++ psm1_listener.m
@@ -1,12 +1,14 @@
-%Listening to Joint State Messages from PSM1
+%Listening for Joint State Messages from PSM1
 %By Adnan Munawar
 node = rosmatlab.node('my_node');
 
+global first_launch;
+first_launch = 1;
 h = rosio;
 handles = guidata(h);
 sub = rosmatlab.subscriber('/dvrk_psm/set_position_joint', ...
-'std_msgs/String', 10, node);
-sub.addCustomMessageListener({@sub_callback,handles});
+'sensor_msgs/JointState', 10, node);
+sub.addCustomMessageListener({@sub_callback, handles});
 pause(2);
 for i=1:100
    disp('waiting');
