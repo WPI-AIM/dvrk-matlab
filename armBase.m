f5075870e237d7ea17a34dd2d13c065d016380bb
Adnan Munawar <amunawar@wpi.edu>
2015-01-26 21:30:50 -0500

    Added baseArm class for defining manipulator types

---
+++ armBase.m
@@ -0,0 +1,14 @@
+classdef armBase
+    properties
+        jnt_pub = [];
+        jnt_sub = [];
+        jnt_msg = []'
+        jnt_state_pub = [];
+        jnt_state_sub = [];
+        n_jnts = [];
+    end
+    
+    methods
+        
+    end
+end
