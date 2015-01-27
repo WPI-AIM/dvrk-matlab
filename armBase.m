fa1f901199286f1ea28dee3c3667a77e03df95d2
Adnan Munawar <amunawar@wpi.edu>
2015-01-27 00:12:44 -0500

    Added functions to armBase class

--- armBase.m
+++ armBase.m
@@ -1,14 +1,36 @@
-classdef armBase
+classdef armBase < handle
     properties
         jnt_pub = [];
         jnt_sub = [];
         jnt_msg = []'
-        jnt_state_pub = [];
-        jnt_state_sub = [];
+        state_pub = [];
+        state_sub = [];
+        state_msg = [];
         n_jnts = [];
+        arm_type = [];
+        node = [];
     end
     
     methods
-        
+        function obj = armBase(node,arm_type)
+            if(strcmp(arm_type,'PSM1') || strcmp(arm_type,'PSM')...
+                   || strcmp(arm_type,'psm1') || strcmp(arm_type,'psm'))
+                obj.arm_type = obj.PSM1;
+                obj.node = node;
+            elseif(strcmp(arm_type,'MTMR') || strcmp(arm_type,'MTM')...
+                   || strcmp(arm_type,'mtmr') || strcmp(arm_type,'mtm'))
+               obj.arm_type = obj.MTMR;
+               obj.node = node;
+            else
+                disp('Error: Arm type not recognized');
+                disp('Valid Arm types are: MTMR MTM mtm mtmr PSM1 PSM psm psm1');
+            end        
+        end
     end
+        
+     enumeration
+         PSM1, MTMR
+     end
+        
 end
+    
